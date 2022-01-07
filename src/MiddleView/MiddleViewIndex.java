414e00a1041bd8836dd9e537d3fc35e5213f69f6
Lirodek <Lirodek.gmail.com>
2022-01-08 08:29:50 +0900

    ready

--- src/MiddleView/MiddleViewIndex.java
+++ src/MiddleView/MiddleViewIndex.java
@@ -9,9 +9,13 @@ public class MiddleViewIndex extends MiddleView {
 	@Override
 	public void display() {
 		getLine("전화번호 목록");
-		Iterator<PhoneInfo> itr = R.aList.iterator();
-		while(itr.hasNext()) {
-			System.out.println(itr.next().toString());
+		try {
+			Iterator<PhoneInfo> itr = R.aList.iterator();
+			while(itr.hasNext()) {
+				System.out.println(itr.next().toString());
+			}
+		} catch (Exception e) {
+			System.out.println("전화목록이 비어있습니다.");
 		}
 		
 	}
